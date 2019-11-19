8b159522d47a2d7605cbe33bb6fc5b5aa3319fdc
kimtaehyung <taehyeong77@naver.com>
2019-11-19 20:04:05 +0900

    commit

--- Qt_Core_IM_6/main.cpp
+++ Qt_Core_IM_6/main.cpp
@@ -1,8 +1,31 @@
 #include <QtCore/QCoreApplication>
 
+#include <qhash.h>
+#include <qdebug.h>
+
+// QHash - QHash<Key, T> is one of Qt's generic container class
+// It stores (key, value) pairs and provides very fast lookup of the value associated with a key
+
+// Works great with static info that wont change mush.
+
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
+
+	QHash<QString, int> ages;
+	ages.insert("Bryan", 44);
+	ages.insert("Aryan", 49);
+	ages.insert("Cryan", 48);
+	ages.insert("Dryan", 46);
+	ages.insert("Qryan", 40);
+
+	qInfo() << "Bryan is " << ages["Bryan"] << "years old";
+	qInfo() << "Keys : " << ages.keys();
+	qInfo() << "Size : " << ages.size();
+
+	foreach(QString key, ages.keys()) {
+		qInfo() << key << " = " << ages[key];
+	}
 
 	return a.exec();
 }
